489b7e18057c092067b6ef85baf9116692fc8f39
NguyenTam2206 <55978864+NguyenTam2206@users.noreply.github.com>
2023-03-30 15:47:50 +0700

    fix middleware movie path

--- routes/movies.js
+++ routes/movies.js
@@ -1,29 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authMiddleware = require('../middlewares/auth');
-const Movie = require('../models/Movie');
+const authMiddleware = require("../middlewares/auth");
+const Movie = require("../models/Movie");
 
-router.use(authMiddleware);
+// router.use(authMiddleware);
 
-router.post('/', async (req, res) => {
-    const { url } = req.body;
-    const username = res.locals.username;
-    const errors = [];
+router.post("/", authMiddleware, async (req, res) => {
+  const { url } = req.body;
+  const username = res.locals.username;
+  const errors = [];
 
-    if (!url) errors.push({ msg: 'url is required' });
-    if (errors.length > 0) return res.json({ code: 0, data: errors });
-    else {
-        const movieData = { url, createdBy: username };
-        console.log({movieData});
-        const movie = new Movie(movieData);
-        await movie.save();
-        return res.json({ code: 1, msg: 'Movie has shared.' });
-    }
-})
+  if (!url) errors.push({ msg: "url is required" });
+  if (errors.length > 0) return res.json({ code: 0, data: errors });
+  else {
+    const movieData = { url, createdBy: username };
+    console.log({ movieData });
+    const movie = new Movie(movieData);
+    await movie.save();
+    return res.json({ code: 1, msg: "Movie has shared." });
+  }
+});
 
-router.get('/', async (req, res) => {
-    const movies = await  Movie.find({});
-    return res.json({ code: 1, data: movies });
-})
+router.get("/", async (req, res) => {
+  const movies = await Movie.find({});
+  return res.json({ code: 1, data: movies });
+});
 
 module.exports = router;
